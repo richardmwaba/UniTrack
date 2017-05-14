1daa8d8ac6e808c40136343ba75c22b0e9fd147e
mumba <chileshemartin@gmail.com>
2017-05-15 01:25:43 +0200

    initial project

---
+++ src/pages/home/home.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+//import { LocationTracker } from '../../providers/location-tracker';
+import { LocationTrackerProvider } from '../../providers/location-tracker/location-tracker';
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  constructor(public navCtrl: NavController, public locationTracker: LocationTrackerProvider) {
+
+  }
+
+  start(){
+    this.locationTracker.startTracking();
+  }
+
+  stop(){
+    this.locationTracker.stopTracking();
+  }
+
+}
